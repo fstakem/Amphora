50eb4a93cc967948fa84d0a8893681d2d92c6fb3
Fred Stakem <fstakem@gmail.com>
2013-09-23 18:29:00 -0400

    Added a basic landing page and the urls for the base pages.

--- randori/randori/urls.py
+++ randori/randori/urls.py
@@ -9,6 +9,25 @@ urlpatterns = patterns('',
 
     # Social auth
     url('', include('social.apps.django_app.urls', namespace='social'))
+
+    # Landing
+    url(r'^$', views.landing, name='randori.views.landing'),
+
+    # Misc
+    url(r'^features$', views.features, name='randori.views.features'),
+    url(r'^blog$', views.blog, name='randori.views.blog'),
+    url(r'^terms$', views.terms, name='randori.views.terms'),
+    url(r'^privacy$', views.privacy, name='randori.views.privacy'),
+    url(r'^about$', views.about, name='randori.views.about'),
+    url(r'^contact$', views.contact, name='randori.views.contact'),
+
+    # Authentication
+    url(r'^login$', views.login, name='randori.views.login'),
+    url(r'^logout$', views.logout, name='randori.views.logout'),
+    url(r'^register$', views.register, name='randori.views.register'),
+
+    # Users
+    url(r'^(?P<user_name>[\w\-]+)', include('social.urls')),
 )
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
