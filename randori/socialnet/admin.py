9bd406c542854f3c7619901678f5efdc23717028
Fred Stakem <fstakem@gmail.com>
2013-09-25 11:10:14 -0400

    Cleaned up the project page.

--- randori/socialnet/admin.py
+++ randori/socialnet/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from socialnet.models import UserProfile
+from socialnet.models import UserProfile, Project
 
 admin.site.register(UserProfile)
+admin.site.register(Project)
