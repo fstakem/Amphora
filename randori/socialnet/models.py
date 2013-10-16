aea15733c9b479eaefd2d878cf237a896ce9b309
Fred Stakem <fstakem@gmail.com>
2013-10-16 19:33:55 -0400

    Got the new db schema mostly working.

--- randori/socialnet/models.py
+++ randori/socialnet/models.py
@@ -21,26 +21,26 @@ def getImagePath(instance, filename, prepend):
     upload_time = str( datetime.datetime.now() )
     upload_time = upload_time.replace(' ', '_').replace('-', '_')
 
-    return os.path.join('photos/', prepend + '/',  str(instance.user.username) + '/', prepend + '__' + upload_time + '__' + filename)
+    return os.path.join('photos/', str(instance.user.username) + '/', prepend + '/',   prepend + '__' + upload_time + '__' + filename)
 
 def getDataPath(instance, filename):
     upload_time = str( datetime.datetime.now() )
     upload_time = upload_time.replace(' ', '_').replace('-', '_')
 
-    return os.path.join('data/project/',  str(instance.project.name) + '/',  'data__' + upload_time + '__' + filename)
+    return os.path.join('data/',  str(instance.owner.username) + '/',  str(instance.host.name) + '/', 'data__' + upload_time + '__' + filename)
 
 
 # Models
 class UserProfile(models.Model):
 	# Attributes
-    web_site = models.URLField(max_length=200, blank=True)
+    web_site = models.URLField(max_length=200, blank=True, null=True)
     profile_photo = models.ImageField(upload_to=getProfilePhotoPath, blank=True, null=True)
     cover_photo = models.ImageField(upload_to=getCoverPhotoPath, blank=True, null=True)
     bio = models.TextField(blank=True)
 
     # Relationships
     user = models.OneToOneField(User, related_name='additional_info')
-    friend = models.ForeignKey("self", blank=True)
+    friend = models.ManyToManyField(User, blank=True, null=True)
     address = models.ManyToManyField('Address', related_name='lives')
 
     def __unicode__(self):
@@ -52,13 +52,13 @@ class Project(models.Model):
     date_created = models.DateTimeField()
     last_activity = models.DateTimeField()
     public = models.BooleanField()
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, null=True)
 
     # Relationships
-    owner = models.ForeignKey('UserProfile', related_name='owned_project')
-    contributor = models.ManyToManyField('UserProfile', related_name='contributed_project')
-    observer = models.ManyToManyField('UserProfile', related_name='watched_project')
-    current_revision = models.OneToOneField('Revision', related_name='active_project')
+    owner = models.ForeignKey(User, related_name='owned_project')
+    contributor = models.ManyToManyField(User, blank=True, null=True, related_name='contributed_project')
+    observer = models.ManyToManyField(User, blank=True, null=True, related_name='watched_project')
+    current_revision = models.OneToOneField('Revision', blank=True, null=True, related_name='active_project')
 
     def __unicode__(self):
         return self.name
@@ -68,12 +68,11 @@ class Revision(models.Model):
 
     # Relationships
     version = models.ForeignKey('Version', related_name='in_revision')
-    host = models.ManyToManyField('Host', related_name='in_revision')
-    project = models.ForeignKey('Project', related_name='previous_revision')
-    software_stack = models.ForeignKey('SoftwareStack', related_name='used_for_revision')
+    project = models.ForeignKey('Project', blank=True, null=True, related_name='previous_revision')
+    software_stack = models.ManyToManyField('SoftwareStack', blank=True, null=True, related_name='used_for_revision')
 
     def __unicode__(self):
-        return str(self.version)
+        return self.version.__unicode__()
 
     def ageComparison(self, other):
         pass
@@ -89,7 +88,7 @@ class Version(models.Model):
     # Relationships
     
     def __unicode__(self):
-        return str(self.major) + '.' + str(self.minor) + '.' + str(self.build)
+        return u'%s.%s.%s' % (str(self.major), str(self.minor), str(self.build))
 
     def ageComparison(self, other):
         pass
@@ -100,7 +99,7 @@ class Host(models.Model):
     description = models.TextField(blank=True)
 
     # Relationships
-    last_location = models.ForeignKey('Location', related_name='current_host')
+    last_location = models.ForeignKey('Location', blank=True, null=True, related_name='current_host')
 
     def __unicode__(self):
         return self.name
@@ -109,13 +108,14 @@ class Data(models.Model):
     # Attributes
     name = models.CharField(max_length=200)
     date_created = models.DateTimeField()
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, null=True)
     file_data = models.FileField(upload_to=getDataPath, blank=True, null=True)
 
     # Relationships
-    owner = models.ForeignKey('UserProfile', related_name='collected_data')
-    location = models.ForeignKey('Location', related_name='collected_data')
-    host = models.ForeignKey('Host', related_name='collected_data')
+    owner = models.ForeignKey(User, related_name='collected_data')
+    revision = models.ForeignKey('Revision', blank=True, null=True, related_name='collected_data')
+    collected_location = models.ForeignKey('Location', blank=True, null=True, related_name='collected_data')
+    host = models.ForeignKey('Host', blank=True, null=True, related_name='collected_data')
 
     def __unicode__(self):
         return self.name
@@ -123,41 +123,47 @@ class Data(models.Model):
 class Location(models.Model):
     # Attributes
     name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
 
     # Relationships
-    sub_location = models.ForeignKey("self", related_name='parent_location', blank=True)
-    address = models.ForeignKey('Address', related_name='location', blank=True)
+    sub_location = models.ManyToManyField("self", blank=True, null=True, related_name='parent_location')
+    address = models.ForeignKey('Address', blank=True, null=True, related_name='location')
 
     def __unicode__(self):
         return self.name
 
 class Address(models.Model):
+    # Vars
+    TYPE_CHOICES = ( ('HOME', 'home'),
+                     ('WORK', 'work'),
+                     ('OTHER', 'other'),)
+
     # Attributes
-    address_type = models.CharField(max_length=200)
-    address_1 = models.CharField(max_length=128, blank=True)                                                           
-    address_2 = models.CharField(max_length=128, blank=True)  
+    address_type = models.CharField(max_length=200, choices=TYPE_CHOICES, default='text')
+    address_1 = models.CharField(max_length=128, blank=True, null=True,)                                                           
+    address_2 = models.CharField(max_length=128, blank=True, null=True,)  
     city = models.CharField(max_length=128)
     state = USStateField(US_STATES)                                                                                                                                           
-    zipcode = models.CharField(max_length=5, blank=True)                                                               
-    zip_plus4 = models.CharField(max_length=4, blank=True) 
+    zipcode = models.CharField(max_length=5, blank=True, null=True,)                                                               
+    zip_plus4 = models.CharField(max_length=4, blank=True, null=True,) 
 
     # Relationships
 
     def __unicode__(self):
-        return self.city + ', ' + self.state
+        return '%s, %s' % (self.city, self.state)
 
 class Analysis(models.Model):
     # Attributes
     name = models.CharField(max_length=200)
     date_created = models.DateTimeField()
     last_activity = models.DateTimeField()
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, null=True)
 
     # Relationships
-    owner = models.ForeignKey('UserProfile', related_name='main_analysis')
-    contributor = models.ManyToManyField('UserProfile', related_name='contributed_analysis')
-    revision = models.ForeignKey('Revision', related_name='data_analysis')
-    data = models.ManyToManyField('Data', related_name='data_analysis')
+    owner = models.ForeignKey(User, related_name='main_analysis')
+    project = models.ForeignKey('Project', related_name='data_analysis')
+    contributor = models.ManyToManyField(User, blank=True, null=True, related_name='contributed_analysis')
+    data = models.ManyToManyField('Data', blank=True, null=True, related_name='data_analysis')
 
     def __unicode__(self):
         return self.name
@@ -168,6 +174,7 @@ class SoftwareStack(models.Model):
 
     # Relationships
     version = models.ForeignKey(Version, related_name='in_software')
+    dependency = models.ManyToManyField("self", blank=True, null=True, related_name='related_dependency')
 
     def __unicode__(self):
         return self.name
--- randori/socialnet/views.py
+++ randori/socialnet/views.py
@@ -116,6 +116,8 @@ def userNewProject(request, user_to_be_viewed, view_type, viewer):
 def userProjects(user_to_be_viewed, view_type, viewer):
 	projects = list( Project.objects.filter(owner__id=user_to_be_viewed.id) )
 
+	print projects
+
 	data = { 'user_name': user_to_be_viewed.username, 
 			 'first_name': user_to_be_viewed.first_name,
 			 'last_name': user_to_be_viewed.last_name,
@@ -132,7 +134,7 @@ def userProjects(user_to_be_viewed, view_type, viewer):
 	return render_to_response('./socialnet/user/projects.html', data)
 
 def userFriends(user_to_be_viewed, view_type, viewer):
-	friends = list( User.objects.filter(friend__id=user_to_be_viewed.id) )
+	friends = list( User.objects.filter(additional_info__friend__id=user_to_be_viewed.id) )
 
 	data = { 'user_name': user_to_be_viewed.username, 
 			 'first_name': user_to_be_viewed.first_name,
@@ -160,20 +162,14 @@ def userContributors(user_to_be_viewed, view_type, viewer):
 	return render_to_response('./socialnet/user/contributors.html', data)
 
 def userData(user_to_be_viewed, view_type, viewer):
-	data_sets = []
-	projects = list( Project.objects.filter(owner__id=user_to_be_viewed.id) )
+	collected_data = list( Data.objects.filter(owner__id=user_to_be_viewed.id) )
 	
-	for project in projects:
-		data_set = list( Data.objects.filter(project__id=project.id) )
-		for d in data_set:
-			data_sets.append( (project, d) )
-
 	data = { 'user_name': user_to_be_viewed.username, 
 			 'first_name': user_to_be_viewed.first_name,
 			 'last_name': user_to_be_viewed.last_name,
 			 'viewer_name': viewer.username,
 			 'view_type': view_type,
-			 'data_sets': data_sets,
+			 'data': collected_data,
 			 'view': 'Data' }
 	
 	if view_type == user_view_types['private']:
