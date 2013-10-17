ecaa399c916017902c166856c0687d13ed9a1c8e
Fred Stakem <fstakem@gmail.com>
2013-10-17 08:29:01 -0400

    Got rid of bugs in the user views.

--- randori/socialnet/urls.py
+++ randori/socialnet/urls.py
@@ -4,7 +4,7 @@ import views
 urlpatterns = patterns('',
                        url(r'^/$', views.user, name='views.user'),
                        url(r'^/(?P<project_name>[\w\-]+)/$', views.project, name='views.project'),
-                       url(r'^/(?P<project_name>[\w\-]+)/data/(?P<data_name>[\w\-]+)/$', views.data, name='views.data'),
+                       url(r'^/(?P<project_name>[\w\-]+)/(?P<host_name>[\w\-]+)/data/(?P<data_name>[\w\-]+)/$', views.data, name='views.data'),
                        url(r'^/(?P<project_name>[\w\-]+)/analysis/(?P<analysis_name>[\w\-]+)/$', views.analysis, name='views.analysis'),
 )
 
