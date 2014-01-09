86eea5cbae5fa9bff83ae02caff08fafd684ddbb
Fred Stakem <fstakem@gmail.com>
2014-01-09 08:51:06 -0500

    Changes to settings

--- randori/randori/settings.py
+++ randori/randori/settings.py
@@ -190,13 +190,19 @@ LOGGING = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config(default='postgres://localhost/randori')
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+# try to load local_settings.py if it exists
+try:
+    from local_settings import *
+except Exception as e:
+    pass
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # END: For Heroku
