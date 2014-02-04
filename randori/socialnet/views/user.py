e358b12760a285f815617ca2f79e392a5c46cda0
Fred Stakem <fstakem@gmail.com>
2014-02-04 12:26:38 -0500

    Finished the rough user profile except for profile images.

--- randori/socialnet/views/user.py
+++ randori/socialnet/views/user.py
@@ -9,9 +9,12 @@
 
 
 # Librarys
+import re
+
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, render
+from django.template import RequestContext
 from django import forms
 from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
@@ -154,23 +157,56 @@ def new_project(request, user_name):
                     'new_project_form': form,
                 }
 
-    if request.POST and form.is_valid():
-        print '------------ POST form bro'
-        name = request.POST.get('name', '')
-        public = request.POST.get('public', '')
-        description = request.POST.get('description', '')
+    if request.POST:
+        cancel = request.POST.get('cancel', '')
+        submit = request.POST.get('submit', '')
 
-        print name
-        print public
-        print description
-        
-        #user = form.login(request)
-        #if user:
-        #    auth.login(request, user)
-        #    return HttpResponseRedirect("/" + user.username)
+        if submit == 'Create' and form.is_valid() and user_view_types['private'] == 'private':
+            name = form.cleaned_data.get('name', '')
+            public = form.cleaned_data.get('public', '')
+            description = form.cleaned_data.get('description', '')
 
-    
+            project = Project(owner=viewer, name=name, public=public, description=description)
+            project.save()
+
+            location = '/' + user_to_be_viewed.username + '/projects'
+            return HttpResponseRedirect(location)
+
+        elif cancel == 'Cancel':
+            location = '/' + user_to_be_viewed.username + '/projects'
+            return HttpResponseRedirect(location)
+
     return render(request, './socialnet/user/new_project.html', page_data)
+
+def delete_project(request, user_name):
+    user_name = filter_username(user_name)
+    subview, view_type, viewer, user_to_be_viewed = get_parameters(request, user_name)
+
+    project_name = request.GET['name']
+    projects = Project.objects.filter(name=project_name)
+   
+    if len(projects) > 0:
+        project = projects[0]
+        project.delete()
+
+    location = '/' + user_to_be_viewed.username + '/projects'
+    
+    return HttpResponseRedirect(location)
+
+def leave_project(request, user_name):
+    user_name = filter_username(user_name)
+    subview, view_type, viewer, user_to_be_viewed = get_parameters(request, user_name)
+
+    project_name = request.GET['name']
+    projects = Project.objects.filter(name=project_name)
+
+    if len(projects) > 0:
+        project = projects[0]
+        project.contributor.remove(user_to_be_viewed.id)
+
+    location = '/' + user_to_be_viewed.username + '/projects'
+
+    return HttpResponseRedirect(location)
 
 def people(request, user_name):
     user_name = filter_username(user_name)
@@ -226,20 +262,65 @@ def settings(request, user_name):
     subview, view_type, viewer, user_to_be_viewed = get_parameters(request, user_name)
 
     if subview == 'account':
-        return account_settings(request, subview, user_to_be_viewed, view_type, viewer)
+        return account_settings(request, user_name)
     elif subview == 'personal':
-        return personal_settings(request, subview, user_to_be_viewed, view_type, viewer)
+        return personal_settings(request, user_name)
     else:
-        return personal_settings(request, subview, user_to_be_viewed, view_type, viewer)
+        subview = 'personal'
+        return personal_settings(request, user_name)
 
-def personal_settings(request, subview, user_to_be_viewed, view_type, viewer):
+def personal_settings(request, user_name):
+    user_name = filter_username(user_name)
+    subview, view_type, viewer, user_to_be_viewed = get_parameters(request, user_name)
+
     user_profile = UserProfile.objects.filter(user__id=user_to_be_viewed.id)[0]
-    print '-----HERE-------'
-    #print request.POST.get('first_name', '')
-    #print request.POST.get('last_name', '')
-    form = ChangeSettingsForm(user_to_be_viewed, user_profile, request.POST or None)
-    #print request.POST.get('first_name', '')
-    #print request.POST.get('last_name', '')
+
+    form = None
+    subview = 'personal'
+    first_name = user_to_be_viewed.first_name
+    last_name = user_to_be_viewed.last_name
+    email = user_to_be_viewed.email
+    website = user_profile.personal_website
+    github_page = user_profile.github_page
+    title = user_profile.title
+
+    form = ChangeSettingsForm(request.POST or None, first_name=first_name, last_name=last_name, 
+                              email=email, website=website,  github_page=github_page, title=title)
+    
+    if request.POST:
+        cancel = request.POST.get('cancel', '')
+        update = request.POST.get('update', '')
+        
+        if update == 'Update' and user_view_types['private'] == 'private':
+           
+            if form.is_valid():
+                user_to_be_viewed.first_name = form.cleaned_data.get('first_name', '')
+                user_to_be_viewed.last_name = form.cleaned_data.get('last_name', '')
+                user_to_be_viewed.email = form.cleaned_data.get('email', '')
+                user_to_be_viewed.save()
+
+                user_profile.personal_website = form.cleaned_data.get('personal_website', '')
+                user_profile.github_page = form.cleaned_data.get('github_page', '')
+                user_profile.title = form.cleaned_data.get('title', '')
+                user_profile.save()
+
+                location = '/' + user_to_be_viewed.username + '/settings?view=personal'
+                return HttpResponseRedirect(location)
+                
+            first_name = request.POST.get('first_name', '')
+            last_name = request.POST.get('last_name', '')
+            email = request.POST.get('email', '')
+            website = request.POST.get('personal_website', '')
+            github_page = request.POST.get('github_page', '')
+            title = request.POST.get('title', '')
+
+            form = ChangeSettingsForm(request.POST or None, first_name=first_name, last_name=last_name, 
+                              email=email, website=website,  github_page=github_page, title=title)
+
+        elif cancel == 'Cancel':
+            location = '/' + user_to_be_viewed.username + '/settings?view=personal'
+            return HttpResponseRedirect(location)
+
     page_data = {
                     'user_name': user_to_be_viewed.username,
                     'first_name': user_to_be_viewed.first_name,
@@ -250,13 +331,13 @@ def personal_settings(request, subview, user_to_be_viewed, view_type, viewer):
                     'subview': subview,
                     'change_settings_form': form,
                 }
-
-    if request.POST and form.is_valid():
-        print 'POST form bro'
     
-    return render_to_response('./socialnet/user/personal_settings.html', page_data)
+    return render_to_response('./socialnet/user/personal_settings.html', page_data, context_instance=RequestContext(request))
 
-def account_settings(request, subview, user_to_be_viewed, view_type, viewer):
+def account_settings(request, user_name):
+    user_name = filter_username(user_name)
+    subview, view_type, viewer, user_to_be_viewed = get_parameters(request, user_name)
+
     page_data = {
                     'user_name': user_to_be_viewed.username,
                     'first_name': user_to_be_viewed.first_name,
@@ -368,38 +449,48 @@ class NewProjectForm(forms.Form):
             Field('public', title="Last name"),
             Field('description', title="Description", placeholder='This is a software development project.'),
             Div( 
-                Reset('cancel', 'Cancel', css_class="button-default"),
-                Submit('submit', 'Submit', css_class="btn-primary"),
+                Submit('cancel', 'Cancel', css_class="btn btn-primary"),
+                Submit('submit', 'Create', css_class="btn btn-success"),
                 css_class='col-lg-offset-2 col-lg-6' 
                 ),
         )
 
     def clean_name(self):
         data = self.cleaned_data['name']
+        filtered_data = filter_username(data)
 
-        print '---CLEANING----'
-        print 'Data: ' + data
-
-        projects = Project.objects.filter(name=data)
+        projects = Project.objects.filter(name=filtered_data)
         if len(projects) > 0:
             raise forms.ValidationError('Project name is taken.')
-        
 
-        return data
+        tokens = filtered_data.strip().split()
+        filtered_data = '_'.join(tokens)
+
+        if not re.match(r'^[a-zA-Z0-9_-]+$', filtered_data):
+            raise forms.ValidationError('AlphaNumeric characters only.')
+
+        return filtered_data
 
 class ChangeSettingsForm(forms.Form):
     first_name = forms.CharField(max_length=200)
     last_name = forms.CharField(max_length=200)
     email = forms.EmailField(max_length=200)
-    personal_website = forms.URLField(max_length=200)
-    github_page = forms.URLField(max_length=200)
+    personal_website = forms.URLField(required=False, max_length=200)
+    github_page = forms.URLField(required=False, max_length=200)
     profile_photo = forms.ImageField(required=False)
     cover_photo = forms.ImageField(required=False)
-    title = forms.CharField(max_length=200)
+    title = forms.CharField(required=False, max_length=200)
     # Dont know how to initialize field
     #bio = forms.CharField(widget=forms.Textarea(attrs={'cols': 20, 'rows': 5}))
     
-    def __init__(self, user, user_profile, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
+        self.first_name = kwargs.pop('first_name')
+        self.last_name = kwargs.pop('last_name')
+        self.email = kwargs.pop('email')
+        self.website = kwargs.pop('website')
+        self.github_page = kwargs.pop('github_page')
+        self.title = kwargs.pop('title')
+
         super(ChangeSettingsForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_id = 'id-change-settings-form'
@@ -413,17 +504,17 @@ class ChangeSettingsForm(forms.Form):
         #ChangeSettingsForm.__dict__['base_fields']['bio'].initial = user_profile.bio
         
         self.helper.layout = Layout(
-            Field('first_name', title="First name", value=user.first_name),
-            Field('last_name', title="Last name", value=user.last_name),
-            Field('email', title="Email", value=user.email),
-            Field('personal_website', title="Personal website", value=user_profile.personal_website),
-            Field('github_page', title="Github page", value=user_profile.github_page),
-            Field('title', title="Title", value=user_profile.title),
+            Field('first_name', title="First name", value=self.first_name),
+            Field('last_name', title="Last name", value=self.last_name),
+            Field('email', title="Email", value=self.email),
+            Field('personal_website', title="Personal website", value=self.website),
+            Field('github_page', title="Github page", value=self.github_page),
+            Field('title', title="Title", value=self.title),
             Field('profile_photo', title="Profile photo"),
             Field('cover_photo', title="Cover photo"),
             #Field('bio', title="Bio", initial=user_profile.bio),
             Div( 
-                Reset('cancel', 'Cancel', css_class="button-default"),
+                Submit('cancel', 'Cancel', css_class="button-default"),
                 Submit('update', 'Update', css_class="btn-success"),
                 css_class='col-lg-offset-2 col-lg-6' 
                 ),
